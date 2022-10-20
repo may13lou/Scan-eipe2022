a477617a8aa4d1009124c8e8a070959b518f45e6
Maya <89120553+may13lou@users.noreply.github.com>
2022-10-20 00:24:48 -0500

    Login Works with specific admin username and password. Shows error message if login fails. (NOT DATABASE VERSION)

--- app/src/main/java/com/example/foodapp/LandingPageActivity.java
+++ app/src/main/java/com/example/foodapp/LandingPageActivity.java
@@ -7,8 +7,16 @@ import android.os.Bundle;
 public class LandingPageActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        //TAKES AWAY TOP BAR
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch(Exception e){}
     }
 }
--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -6,54 +6,115 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
 
 public class MainActivity extends AppCompatActivity
 {
-    //BUTTON LINKING TO CREATE LOGIN SCREEN
-    private Button signUpButton;
+    /* INITIALIZE */
+    private Button signUpButtonMain;
+    private Button loginButtonMain;
+    private EditText usernameMain;
+    private EditText passwordMain;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        //START UP DEFAULT -> DO NOT DELETE
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        /* MAIN PAGE SET-UP */
 
-        //TAKES AWAY TOP BAR
-        try
-        {
-            this.getSupportActionBar().hide();
-        }
-        catch(Exception e){};
+            //START UP DEFAULT -> DO NOT DELETE
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-        //CODE TO LINK TO NEW USER
-        signUpButton = (Button) findViewById(R.id.signup_button);
-        signUpButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
+            //TAKES AWAY TOP BAR
+            try
             {
-                openNewUserActivity();
+                this.getSupportActionBar().hide();
             }
-        });
+            catch(Exception e) {
+                e.printStackTrace();
+            }
 
-        //ANIMATIONS WITH YoYo LIBRARY -> CHECK DEPENDENCIES
-        YoYo.with(Techniques.Bounce)
-                .duration(1800)
-                .repeat(100)
-                .playOn(findViewById(R.id.signup_button));
+            //ANIMATIONS WITH YoYo LIBRARY -> CHECK DEPENDENCIES
+            YoYo.with(Techniques.Bounce)
+                    .duration(1800)
+                    .repeat(100)
+                    .playOn(findViewById(R.id.signup_button));
 
+        /* END OF MAIN PAGE SET-UP */
+
+
+
+        /* VARIABLES AND INITIALIZATION (MUST BE AFTER PAGE SETUP) */
+        
+            usernameMain = findViewById(R.id.stringUserName);
+            passwordMain= findViewById(R.id.stringPassword);
+            loginButtonMain = findViewById(R.id.login_main_button);
+            signUpButtonMain = findViewById(R.id.signup_button);
+
+        /* END OF VARIABLES AND INITIALIZATION */
+
+
+
+        /* LINKING TO PAGES */
+
+            //CODE TO SIGNUP BUTTON LINK TO NEW USER
+            signUpButtonMain.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+                    openNewUserActivity();
+                }
+            });
+
+            //CODE TO LOGIN BUTTON TO GET INTO HOME SCREEN
+            loginButtonMain.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+                    checkLogin(usernameMain.getText().toString(),passwordMain.getText().toString());
+                }
+            });
+
+        /* END OF LINKING TO PAGES */
     }
 
-    //CODE TO LINK TO NEW USER
+
+
+    /* FUNCTIONS */
+    //FUNCTION TO LINK TO NEW USER ACTIVITY
     public void openNewUserActivity()
     {
         Intent intent = new Intent(this, NewUserActivity.class);
         startActivity(intent);
     }
 
+    //FUNCTION TO LINK TO HOME SCREEN ACTIVITY  NOTE: FOR NOW FOR LINKING PURPOSES I LINKED IT TO LANDING PAGE
+    public void openHomeActivity()
+    {
+        Intent intent = new Intent(this, LandingPageActivity.class);
+        startActivity(intent);
+    }
+
+    //LOGIN VALIDATION FUNCTION
+    public void checkLogin(String userName, String userPassword)
+    {
+        /* NOTE: user = "admin" and password = "testing" for login to work */
+        if((userName.equals("admin")) && (userPassword.equals("testing")))
+        {
+            openHomeActivity();
+        }
+        else
+        {
+            Toast.makeText(MainActivity.this, "Username or Password is incorrect. Please try again.",Toast.LENGTH_SHORT).show();
+        }
+    }
 
 }
